21906c0e5e6ac6e5019a94cc2d4d7ad95ea40ad8
JimHans <31241581+JimHans@users.noreply.github.com>
2022-04-05 18:44:28 +0800

    Fixed 404

--- sw.js
+++ sw.js
@@ -2,9 +2,9 @@ const CACHE_NAME = 'uestx_cache';
  
 const URLS = [                // Add URL you want to cache in this list.
   // '/',                     // If you have separate JS/CSS files, add path to those files here
-  './index.html',
-  './UESTX_files/animate.css',
-  './UESTX_files/jquery.js'
+  'https://jimhans.github.io/uestc-uestx/index.html',
+  'https://jimhans.github.io/uestc-uestx/UESTX_files/animate.css',
+  'https://jimhans.github.io/uestc-uestx/UESTX_files/jquery.js'
 ];
  
 // Cache resources
