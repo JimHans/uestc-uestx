15ccc980755ac8db01c77dd04e9700f0eb5fb6cd
JimHans <31241581+JimHans@users.noreply.github.com>
2022-04-05 18:40:45 +0800

    Bug Fixes

--- sw.js
+++ sw.js
@@ -2,9 +2,9 @@ const CACHE_NAME = 'uestx_cache';
  
 const URLS = [                // Add URL you want to cache in this list.
   // '/',                     // If you have separate JS/CSS files, add path to those files here
-  '/index.html',
-  '/UESTX_files/animate.css',
-  '/UESTX_files/jquery.js'
+  './index.html',
+  './UESTX_files/animate.css',
+  './UESTX_files/jquery.js'
 ];
  
 // Cache resources
