005f682358345d988fd6538714c0a6ee19d87d52
JimHans <31241581+JimHans@users.noreply.github.com>
2022-04-09 00:56:41 +0800

    Fixed PWA Bugs

--- sw.js
+++ sw.js
@@ -18,3 +18,38 @@ self.addEventListener('install', function (e) {
     }
   ))
 });
+
+self.addEventListener("fetch", async event => {
+  const req = event.request;
+  const url = new URL(req.url);
+
+  if (url.origin === location.origin) {
+    event.respondWith(cacheFirst(req));
+  } else {
+    event.respondWith(networkFirst(req));
+  }
+});
+
+async function cacheFirst(req) {
+  const cachedResponse = await caches.match(req);
+  return cachedResponse || fetch(req);
+}
+
+async function networkFirst(req) {
+  const cache = await caches.open("topics-dynamic");
+  try {
+    const res = await fetch(req);
+    cache.put(req, res.clone());
+    return res;
+  } catch (error) {
+    return await cache.match(req);
+  }
+}
+
+self.addEventListener("install", async event => {
+  console.log('sw install');
+});
+
+self.addEventListener("fetch", async event => {
+  console.log('sw fetch');
+});
