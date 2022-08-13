583ee866c57fa118eefdebada772cfebf785d2e8
JimHans <31241581+JimHans@users.noreply.github.com>
2022-08-13 17:32:58 +0800

    Fixed sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,17 @@ const URLS = [                // Add URL you want to cache in this list.
   // '/',                     // If you have separate JS/CSS files, add path to those files here
   './index.htm',
   './UESTX_files/animate.css',
-  './UESTX_files/jquery.js'
+  './UESTX_files/jquery.js',
+  './UESTX_files/pic/campus.jpg',
+  './UESTX_files/pic/grade.jpg',
+  './UESTX_files/pic/dachuang.jpg',
+  './UESTX_files/pic/courses.jpg',
+  './UESTX_files/pic/mail.jpg',
+  './UESTX_files/pic/uestc.png',
+  './UESTX_files/pic/jzsz.jpg',
+  './UESTX_files/pic/jwc.jpg',
+  './UESTX_files/pic/login.jpg',
+  './UESTX_files/pic/bg-main.png',
 ];
  
 // Cache resources
